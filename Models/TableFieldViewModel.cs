c6710a8c6b5c98bf985f863490e7921e918904e7
AlanHO0702 <alan@jsis.com.tw>
2025-07-11 16:36:15 +0800

    獨立單據樣板

---
+++ Models/TableFieldViewModel.cs
@@ -0,0 +1,7 @@
+    public class TableFieldViewModel
+    {
+        public string FieldName { get; set; }
+        public string DisplayLabel { get; set; }
+        public int SerialNum { get; set; }
+        public bool Visible { get; set; }
+    }
---
+++ Pages/SpodOrderMain.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net.Http.Json;
+using PcbErpApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+public class SpodOrdersModel : TableListModel<SpodOrderMain>
+{
+
+    public SpodOrdersModel(IHttpClientFactory httpClientFactory, ITableDictionaryService dictService)
+        : base(httpClientFactory, dictService) { }
+
+    public override string TableName => "SpodOrderMain";
+
+}
